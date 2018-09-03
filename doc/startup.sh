15cde824373ba89df07d4561bd9aabd07c33ef8c
Vietor Liu <vietor.liu@gmail.com>
2018-09-03 17:22:52 +0800

    Add demo startup script

---
+++ doc/startup.sh
@@ -0,0 +1,12 @@
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do
+  DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null && pwd )"
+
+PARAMS="" # -d data
+
+cd ${DIR}
+bin/tinynfsd ${PARAMS} &>logs/output.txt &
